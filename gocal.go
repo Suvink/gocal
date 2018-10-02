7947546d995aee2ef363441d9a51577b1c029c7d
Suvin Nimnaka <suvin@richmun.org>
2018-10-02 15:23:27 +0530

    Update gocal.go

--- gocal.go
+++ gocal.go
@@ -7,10 +7,8 @@ import "fmt"
 //declaration of variables
 var num1 float32
 var num2 float32
-var num3 float32
 var operator string
 var view bool
-var mode string
 
 
 //declaration of functions
@@ -34,54 +32,34 @@ func divide(a float32, b float32)float32{
 func main(){
 
 
+fmt.Print("Enter No 1: ")
+fmt.Scanln(&num1)
 
-fmt.Print("Select a mode. Simple | Advanced ")
-fmt.Scanln(&mode)
-if(mode == "advanced"){
+fmt.Print("Enter No 2: ")
+fmt.Scanln(&num2)
 
-      fmt.Print("Enter a number to find the square:  ")
-      fmt.Scanln(&num3)
-      fmt.Print("Your answer is: ")
-      fmt.Println(multiply(num3,num3))
-      fmt.Println("\n")
-  
+fmt.Print("Enter operator: ")
+fmt.Scanln(&operator)
 
+if (operator == "+"){
+  fmt.Print("Your answer is: ")
+  fmt.Println(add(num1,num2))
+  fmt.Println("\n")
 
-}else if(mode == "simple"){
+}else if (operator == "-"){
+  fmt.Print("Your answer is: ")
+  fmt.Println(substract(num1,num2))
+  fmt.Println("\n")
 
-      fmt.Print("Enter No 1: ")
-      fmt.Scanln(&num1)
+}else if (operator == "*"){
+  fmt.Print("Your answer is: ")
+  fmt.Println(multiply(num1,num2))
+  fmt.Println("\n")
 
-      fmt.Print("Enter No 2: ")
-      fmt.Scanln(&num2)
-
-      fmt.Print("Enter operator: ")
-      fmt.Scanln(&operator)
-
-      if (operator == "+"){
-        fmt.Print("Your answer is: ")
-        fmt.Println(add(num1,num2))
-        fmt.Println("\n")
-
-      }else if (operator == "-"){
-        fmt.Print("Your answer is: ")
-        fmt.Println(substract(num1,num2))
-        fmt.Println("\n")
-
-      }else if (operator == "*"){
-        fmt.Print("Your answer is: ")
-        fmt.Println(multiply(num1,num2))
-        fmt.Println("\n")
-
-      }else if (operator == "/"){
-        fmt.Print("Your answer is: ")
-        fmt.Println(divide(num1,num2))
-        fmt.Println("\n")
-      }
-
-    }
-
-
-
+}else if (operator == "/"){
+  fmt.Print("Your answer is: ")
+  fmt.Println(divide(num1,num2))
+  fmt.Println("\n")
+}
 
 }
